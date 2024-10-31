bb8a94fcb7a1b0b99595cc56429ebe73a11436fa
Valeriya <113857212+Velsee@users.noreply.github.com>
2024-10-31 13:34:54 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -3,7 +3,7 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):
+    def go_to(self, new_floor: int):
         if 0 < new_floor <= self.number_of_floors:
             # Вывод последовательности чисел от 1 до new_floor
             for floor in range(1, new_floor + 1):
